ab11c0b482f1990fddb188ca682744751ed2b26b
Ravindu Wanshanatha <63859006+ravindu568@users.noreply.github.com>
2024-05-11 23:38:38 +0530

    5

--- src/app/modules/user/component/user-context/user-context.component.ts
+++ src/app/modules/user/component/user-context/user-context.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-user-context',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+    NavbarComponent
+  ],
   templateUrl: './user-context.component.html',
   styleUrl: './user-context.component.scss'
 })
--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UserHomeComponent} from "./component/user-home/user-home.component";
+import {UserDryfoodComponent} from "./component/user-dryfood/user-dryfood.component";
+import {UserFreshffoodComponent} from "./component/user-freshffood/user-freshffood.component";
+import {UserMeatLockerComponent} from "./component/user-meat-locker/user-meat-locker.component";
 
 const routes: Routes = [
   {path:'',redirectTo:'/user/process/home',pathMatch:'full'},
   {path:'process',loadComponent:()=>import('./component/user-context/user-context.component').then(c=>c.UserContextComponent),
   children:[
-    {path:'home',component:UserHomeComponent}
+    {path:'home',component:UserHomeComponent},
+    {path:'dry-food',component:UserDryfoodComponent},
+    {path:'fresh-food',component:UserFreshffoodComponent},
+    {path:'meat-locker',component:UserMeatLockerComponent}
+
+
   ]
   }
 ];
