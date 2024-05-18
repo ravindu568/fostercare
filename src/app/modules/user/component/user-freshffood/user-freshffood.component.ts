b16477aee76f81fdc408f9677b24b1fc5808a82d
Ravindu Wanshanatha <63859006+ravindu568@users.noreply.github.com>
2024-05-18 09:54:56 +0530

    7

--- src/app/modules/user/component/user-freshffood/user-freshffood.component.ts
+++ src/app/modules/user/component/user-freshffood/user-freshffood.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatFabButton} from "@angular/material/button";
 import {MatPaginator} from "@angular/material/paginator";
+import {MatIcon} from "@angular/material/icon";
+import {MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-user-freshffood',
   standalone: true,
   imports: [
     MatButton,
-    MatPaginator
+    MatPaginator,
+    MatIcon,
+    MatFabButton,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatHint,
+
   ],
   templateUrl: './user-freshffood.component.html',
   styleUrl: './user-freshffood.component.scss'
--- src/app/modules/user/component/user-meat-locker/user-meat-locker.component.ts
+++ src/app/modules/user/component/user-meat-locker/user-meat-locker.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatPaginator} from "@angular/material/paginator";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-user-meat-locker',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatPaginator,
+    RouterLink
+  ],
   templateUrl: './user-meat-locker.component.html',
   styleUrl: './user-meat-locker.component.scss'
 })
